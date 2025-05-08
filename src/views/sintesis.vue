c3dc81087ed99025a6fbd6c64a896d30d7bc2c83
Leyson Castaño <leysofcp@hotmail.com>
2025-05-08 01:43:45 -0500

    Update sintesis.vue

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -6,7 +6,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5.bgs.p-4.brad
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src="@/assets/curso/sintesis.svg", alt="Síntesis sobre fundamentos de matemáticas financieras que presenta conceptos clave como valor del dinero en el tiempo, interés simple y compuesto, tipos de tasas de interés, elementos y métodos de amortización, alternativas de inversión como VPN y TIR, y principios de la NIIF 13 sobre valor razonable.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
